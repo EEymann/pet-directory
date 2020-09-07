4c65c15cb24a1698d0d1d4ca6d01bbfa0e82e63e
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-09-07 13:52:33 -0700

    change some data in pets.js.

--- js/pets.js
+++ js/pets.js
@@ -1,38 +1,38 @@
 /*
   Create an array of 'pet' objects.
-  Each object should have the following properties: 
+  Each object should have the following properties:
   name, type, breed, age, and photo
 */
 const pets = [
   {
-    name: 'Joey',
+    name: 'Patches',
     type: 'Dog',
     breed: 'Australian Shepherd',
-    age: 8,
+    age: 2,
     photo: 'img/aussie.jpg'
   },
-  { 
-    name: 'Patches',
+  {
+    name: 'Francis',
     type: 'Cat',
     breed: 'Domestic Shorthair',
     age: 1,
     photo: 'img/tabby.jpg'
   },
-  { 
+  {
     name: 'Pugsley',
     type: 'Dog',
     breed: 'Pug',
-    age: 6,
+    age: 1,
     photo: 'img/pug.jpg'
   },
-  { 
-    name: 'Simba',
+  {
+    name: 'Fluffy',
     type: 'Cat',
     breed: 'Persian',
     age: 5,
     photo: 'img/persian.jpg'
   },
-  { 
+  {
     name: 'Comet',
     type: 'Dog',
     breed: 'Golden Retriever',
